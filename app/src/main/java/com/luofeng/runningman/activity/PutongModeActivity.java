8f73e1c8efaec080ec42e134bd5c8b088107dbc8
luofeng <luofeng19940717@163.com>
2016-08-06 01:04:59 +0800

    can locate, but can't locate twice

--- app/src/main/java/com/luofeng/runningman/activity/PutongModeActivity.java
+++ app/src/main/java/com/luofeng/runningman/activity/PutongModeActivity.java
@@ -2,33 +2,160 @@ package com.luofeng.runningman.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-import com.amap.api.maps2d.LocationSource;
-import com.amap.api.maps2d.LocationSource.OnLocationChangedListener;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.AMap;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.Loca
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.AMap;
 
 
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MyLocationStyle;
 import com.luofeng.runningman.R;
 
 /**
  * Created by 罗峰 on 2016/8/4.
  */
-public class PutongModeActivity extends Activity {
+public class PutongModeActivity extends Activity implements LocationSource, AMapLocationListener {
     private MapView mapView;
     private AMap aMap;
-    private LocationManagerProxy mLocationmanagerProxy;
-    private OnLocationChangedListener mListener;
+    private AMapLocationClient mLocationClient = null;//定位发起端
+    private AMapLocationClientOption mLocationClientOption = null;//定位参数
+    private OnLocationChangedListener mListener = null;//定位监听器
+
+    private boolean isFirstLoc = true;
 
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.putong_mode_layout);
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+        init();
+        initLoc();
     }
+
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        setUpMap();
+    }
+
+    private void setUpMap() {
+        aMap.setLocationSource(this);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        //定位的小图标 默认是蓝点 这里自定义一团火，其实就是一张图片
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.strokeWidth(0);
+        aMap.setMyLocationStyle(myLocationStyle);
+    }
+
+    private void initLoc() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化定位参数
+        mLocationClientOption = new AMapLocationClientOption();
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationClientOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationClientOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationClientOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationClientOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationClientOption.setInterval(5000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationClientOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精度信息
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getStreet();//街道信息
+                aMapLocation.getStreetNum();//街道门牌号信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+                Log.d("test", Double.toString(aMapLocation.getLongitude())+","+Double.toString(aMapLocation.getLatitude()));
+                if (isFirstLoc) {
+                    //设置缩放级别
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+                    //将地图移动到定位点
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                    //点击定位按钮 能够将地图的中心移动到定位点
+                    mListener.onLocationChanged(aMapLocation);
+                    //添加图钉
+                    //aMap.addMarker(getMarkerOptions(aMapLocation));
+                    //获取定位信息
+                    StringBuffer buffer = new StringBuffer();
+                    buffer.append(aMapLocation.getCountry() + "" + aMapLocation.getProvince() + "" + aMapLocation.getCity() + "" + aMapLocation.getProvince() + "" + aMapLocation.getDistrict() + "" + aMapLocation.getStreet() + "" + aMapLocation.getStreetNum());
+                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_SHORT).show();
+                    isFirstLoc = false;
+                }
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+
+                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+    //激活定位
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+    }
+    //停止定位
+    @Override
+    public void deactivate() {
+        mListener = null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
