9f60a95c84330d62a03876475476228f8cf4989f
luofeng <luofeng19940717@163.com>
2016-08-06 21:47:52 +0800

    can locate more than twice

--- app/src/main/java/com/luofeng/runningman/activity/PutongModeActivity.java
+++ app/src/main/java/com/luofeng/runningman/activity/PutongModeActivity.java
@@ -1,9 +1,9 @@
 package com.luofeng.runningman.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.amap.api.location.AMapLocationClient;
@@ -16,122 +16,157 @@ import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.AMap;
 
 
+import com.amap.api.maps2d.UiSettings;
 import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.MyLocationStyle;
 import com.luofeng.runningman.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by 罗峰 on 2016/8/4.
  */
 public class PutongModeActivity extends Activity implements LocationSource, AMapLocationListener {
-    private MapView mapView;
-    private AMap aMap;
+    //显示地图需要的变量
+    private MapView mapView;//地图控件
+    private AMap aMap;//地图对象
+
+
+    //定位需要的声明
     private AMapLocationClient mLocationClient = null;//定位发起端
-    private AMapLocationClientOption mLocationClientOption = null;//定位参数
+    private AMapLocationClientOption mLocationOption = null;//定位参数
     private OnLocationChangedListener mListener = null;//定位监听器
 
+    //标识，用于判断是否只显示一次定位信息和用户重新定位
     private boolean isFirstLoc = true;
 
-    protected  void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.putong_mode_layout);
+
+        //显示地图
         mapView = (MapView) findViewById(R.id.map);
+        //必须要写
         mapView.onCreate(savedInstanceState);
-        init();
-        initLoc();
-    }
+        //获取地图对象
+        aMap = mapView.getMap();
 
-    private void init() {
-        if (aMap == null) {
-            aMap = mapView.getMap();
-        }
-        setUpMap();
-    }
 
-    private void setUpMap() {
+        //设置显示定位按钮 并且可以点击
+        UiSettings settings = aMap.getUiSettings();
+        //设置定位监听
         aMap.setLocationSource(this);
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        // 是否显示定位按钮
+        settings.setMyLocationButtonEnabled(true);
+        // 是否可触发定位并显示定位层
         aMap.setMyLocationEnabled(true);
+
+
         //定位的小图标 默认是蓝点 这里自定义一团火，其实就是一张图片
         MyLocationStyle myLocationStyle = new MyLocationStyle();
-        myLocationStyle.strokeWidth(0);
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.icon1));
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
         aMap.setMyLocationStyle(myLocationStyle);
+
+        //开始定位
+        initLoc();
+
+
     }
 
+
+    //定位
     private void initLoc() {
         //初始化定位
         mLocationClient = new AMapLocationClient(getApplicationContext());
         //设置定位回调监听
         mLocationClient.setLocationListener(this);
         //初始化定位参数
-        mLocationClientOption = new AMapLocationClientOption();
+        mLocationOption = new AMapLocationClientOption();
         //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
         //设置是否返回地址信息（默认返回地址信息）
-        mLocationClientOption.setNeedAddress(true);
+        mLocationOption.setNeedAddress(true);
         //设置是否只定位一次,默认为false
-        mLocationClientOption.setOnceLocation(false);
+        mLocationOption.setOnceLocation(false);
         //设置是否强制刷新WIFI，默认为强制刷新
-        mLocationClientOption.setWifiActiveScan(true);
+        mLocationOption.setWifiActiveScan(true);
         //设置是否允许模拟位置,默认为false，不允许模拟位置
-        mLocationClientOption.setMockEnable(false);
+        mLocationOption.setMockEnable(false);
         //设置定位间隔,单位毫秒,默认为2000ms
-        mLocationClientOption.setInterval(5000);
+        mLocationOption.setInterval(2000);
         //给定位客户端对象设置定位参数
-        mLocationClient.setLocationOption(mLocationClientOption);
+        mLocationClient.setLocationOption(mLocationOption);
         //启动定位
         mLocationClient.startLocation();
     }
 
 
-    public void onLocationChanged(AMapLocation aMapLocation) {
-        if (aMapLocation != null) {
-            if (aMapLocation.getErrorCode() == 0) {
-                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
-                aMapLocation.getLatitude();//获取纬度
-                aMapLocation.getLongitude();//获取经度
-                aMapLocation.getAccuracy();//获取精度信息
-                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
-                aMapLocation.getCountry();//国家信息
-                aMapLocation.getProvince();//省信息
-                aMapLocation.getCity();//城市信息
-                aMapLocation.getDistrict();//城区信息
-                aMapLocation.getStreet();//街道信息
-                aMapLocation.getStreetNum();//街道门牌号信息
-                aMapLocation.getCityCode();//城市编码
-                aMapLocation.getAdCode();//地区编码
-                Log.d("test", Double.toString(aMapLocation.getLongitude())+","+Double.toString(aMapLocation.getLatitude()));
+    //定位回调函数
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(amapLocation.getTime());
+                df.format(date);//定位时间
+                amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                amapLocation.getCountry();//国家信息
+                amapLocation.getProvince();//省信息
+                amapLocation.getCity();//城市信息
+                amapLocation.getDistrict();//城区信息
+                amapLocation.getStreet();//街道信息
+                amapLocation.getStreetNum();//街道门牌号信息
+                amapLocation.getCityCode();//城市编码
+                amapLocation.getAdCode();//地区编码
+
+                // 如果不设置标志位，此时再拖动地图时，它会不断将地图移动到当前的位置
                 if (isFirstLoc) {
                     //设置缩放级别
-                    aMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(19));
                     //将地图移动到定位点
-                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude())));
                     //点击定位按钮 能够将地图的中心移动到定位点
-                    mListener.onLocationChanged(aMapLocation);
+                    mListener.onLocationChanged(amapLocation);
                     //添加图钉
-                    //aMap.addMarker(getMarkerOptions(aMapLocation));
+                    //aMap.addMarker(getMarkerOptions(amapLocation));
                     //获取定位信息
                     StringBuffer buffer = new StringBuffer();
-                    buffer.append(aMapLocation.getCountry() + "" + aMapLocation.getProvince() + "" + aMapLocation.getCity() + "" + aMapLocation.getProvince() + "" + aMapLocation.getDistrict() + "" + aMapLocation.getStreet() + "" + aMapLocation.getStreetNum());
-                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_SHORT).show();
-                    isFirstLoc = false;
+                    buffer.append(amapLocation.getCountry() + "" + amapLocation.getProvince() + "" + amapLocation.getCity() + "" + amapLocation.getProvince() + "" + amapLocation.getDistrict() + "" + amapLocation.getStreet() + "" + amapLocation.getStreetNum());
+                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+                    isFirstLoc = true;
                 }
+
+
             } else {
                 //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
                 Log.e("AmapError", "location Error, ErrCode:"
-                        + aMapLocation.getErrorCode() + ", errInfo:"
-                        + aMapLocation.getErrorInfo());
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
 
-                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
-
+                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
     //激活定位
-    public void activate(OnLocationChangedListener onLocationChangedListener) {
-        mListener = onLocationChangedListener;
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+
     }
+
     //停止定位
     @Override
     public void deactivate() {
@@ -139,45 +174,39 @@ public class PutongModeActivity extends Activity implements LocationSource, AMap
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mapView.onDestroy();
-    }
+    /**
+     * 方法必须重写
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
+
+    /**
+     * 方法必须重写
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
+
+    /**
+     * 方法必须重写
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
         mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
